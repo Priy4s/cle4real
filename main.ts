00b0b4e1e05372119577c9191291b67a67a81abc
Priy4s <94791821+Priy4s@users.noreply.github.com>
2024-04-23 14:24:37 +0200

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,48 @@
+namespace SpriteKind {
+    export const Object = SpriteKind.create()
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (!(playing)) {
         if (level == 1) {
             game.splash("\"Level 1 - The start\"")
+            chest = sprites.create(img`
+                . . b b b b b b b b b b b b . . 
+                . b e 4 4 4 4 4 4 4 4 4 4 e b . 
+                b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+                b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+                b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+                b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+                b e e e e e e e e e e e e e e b 
+                b e e e e e e e e e e e e e e b 
+                b b b b b b b d d b b b b b b b 
+                c b b b b b b c c b b b b b b c 
+                c c c c c c b c c b c c c c c c 
+                b e e e e e c b b c e e e e e b 
+                b e e e e e e e e e e e e e e b 
+                b c e e e e e e e e e e e e c b 
+                b b b b b b b b b b b b b b b b 
+                . b b . . . . . . . . . . b b . 
+                `, SpriteKind.Object)
+            key = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . 5 5 5 . . . . . . . . . . . 
+                . 5 5 5 5 5 . . . . . . . . . . 
+                5 5 . . 5 5 5 5 5 5 5 5 5 5 5 5 
+                5 5 . . 5 5 5 5 5 5 5 5 5 5 5 5 
+                5 5 . . 5 5 . . . . . . 5 . 5 . 
+                . 5 5 5 5 5 . . . . . . 5 . 5 . 
+                . . 5 5 5 . . . . . . . 5 . 5 . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.Player)
+            chest.setPosition(20, 25)
+            key.setPosition(38, 25)
         }
         if (level == 2) {
             game.splash("\"Level 2 - The journey\"")
@@ -50,6 +91,8 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         tiles.setCurrentTilemap(tilemap`level15`)
     }
 })
+let key: Sprite = null
+let chest: Sprite = null
 let mySprite: Sprite = null
 let level = 0
 let playing = false
